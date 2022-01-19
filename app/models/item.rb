2a8da736b2107acd46b966105f5024b2ecce2206
s-soichiro <sakamoto.soichiro@gmail.com>
2022-01-19 15:52:10 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefectures
+  belongs_to :delivery_date
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :price, presence: true
+
+  validates :category_id	, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefectures_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_date_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
