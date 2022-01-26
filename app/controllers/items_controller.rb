f9a11d9d15b7874b479d6616dd43ecb143cb8035
s-soichiro <sakamoto.soichiro@gmail.com>
2022-01-26 16:06:25 +0900

    JS価格表示実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,13 @@ class ItemsController < ApplicationController
     if user_signed_in?
       @items = Item.new
     else
-      redirect_to root_path
+      redirect_to new_user_session_path
     end
+  end
+
+  def create
+
+
   end
 
 end
