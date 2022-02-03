429c08fbf49601f41fbf7ce7905e48b686add91d
s-soichiro <sakamoto.soichiro@gmail.com>
2022-02-03 18:01:43 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,12 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
 
   def create
-    @items = Item.new(item_params)
-    if @items.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
