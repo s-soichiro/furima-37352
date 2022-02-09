e926ff16fa99a529af5e06863f9cf08d6fa51d66
s-soichiro <sakamoto.soichiro@gmail.com>
2022-02-09 15:23:00 +0900

    決済情報保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
 
   def index
-    @item = Item.find(params[:item_id])
+    
   end
 
   def create
-    pay_item
-    redirect_to root_path
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      redirect_to root_path
+      @order.save
+    else
+      render :index
+    end
   end
+
 
   private
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
@@ -20,4 +28,13 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     )
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.permit(:token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :item
+
+  
   attr_accessor :token
   validates :token, presence: true
 end
