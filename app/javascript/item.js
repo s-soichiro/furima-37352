890ae920c343d36eee9c3eb8fcb5dcd7d39e79a6
s-soichiro <sakamoto.soichiro@gmail.com>
2022-02-11 12:12:54 +0900

    利益計算修正

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -3,8 +3,8 @@ function item (){
   price.addEventListener("keyup", () => {
     const tax  = document.getElementById('add-tax-price');
     const profit  = document.getElementById('profit');
-    tax.innerHTML = `${price.value * 0.1}`;
-    profit.innerHTML = `${price.value * 0.9}`;
+    tax.innerHTML = `${Math.round(price.value * 0.1)}`;
+    profit.innerHTML = `${Math.round(price.value * 0.9)}`;
   });
 };
 
